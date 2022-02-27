310b4c2cbb3188ff4e9afc6e97ddf242c9ba5d42
Prawploy Techadanai <todaykat@gmail.com>
2022-02-28 00:37:13 +0700

    build success without tests #4

--- src/test/java/com/example/week1/Week1ApplicationTests.java
+++ src/test/java/com/example/week1/Week1ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.week1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Week1ApplicationTests.class)
 class Week1ApplicationTests {
 
 	@Test
