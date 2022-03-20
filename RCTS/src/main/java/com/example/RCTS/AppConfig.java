9635e3f84b019919a5ae48da966860b55c2a87cd
Prawploy Techadanai <todaykat@gmail.com>
2022-03-20 13:30:08 +0700

    print bean count #6

---
+++ RCTS/src/main/java/com/example/RCTS/AppConfig.java
@@ -0,0 +1,18 @@
+package com.example.hello;
+//to config rest api here
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean //can only be  used with methods
+    public RestTemplate createRestTemplate(){
+        //to build resttemplate
+        return new RestTemplateBuilder().build();
+    }
+
+}
--- RCTS/src/main/java/com/example/RCTS/RctsApplication.java
+++ RCTS/src/main/java/com/example/RCTS/RctsApplication.java
@@ -2,12 +2,17 @@ package com.example.RCTS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RctsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RctsApplication.class, args);
+		ConfigurableApplicationContext context =
+				SpringApplication.run(RctsApplication.class, args);
+
+		int count = context.getBeanDefinitionCount();
+		System.out.println(count);
 	}
 
 }
