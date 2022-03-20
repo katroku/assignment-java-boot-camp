fbf0d6a7a8e8d3fc479864854dbc051132e074e5
Prawploy Techadanai <todaykat@gmail.com>
2022-03-20 12:57:15 +0700

    download Spring Boot template with dependencies #6

---
+++ RCTS/src/test/java/com/example/RCTS/RctsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RCTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RctsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
