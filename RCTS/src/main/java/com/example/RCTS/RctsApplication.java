b997ed63694f674417ce8feef3c9ff6b9e91b01c
Prawploy Techadanai <todaykat@gmail.com>
2022-03-21 18:40:55 +0700

    Revert "add cashbox api test #9"
    This reverts commit dcf2ce9eb0c37c05b13bb92206c9213aa9bc0a2d.

--- RCTS/src/main/java/com/example/RCTS/RctsApplication.java
+++ RCTS/src/main/java/com/example/RCTS/RctsApplication.java
@@ -29,6 +29,21 @@ public class RctsApplication {
 	@PostConstruct //after init all beans
 	public void initializeData(){
 
+		double[] latLong = new double[]{123.4, 123.5};
+		Geo location = new Geo(1,latLong); //don't need info about cashboxes or cashbox status
+		System.out.println(location.getCoordinates()[0]);
+        System.out.println(location.getCoordinates()[1]);
+		geoRepository.save(location);
+
+		CashBox cashBoxTest = new CashBox();
+		cashBoxTest.setLocation(location);
+		cashBoxTest.setStatusList(new ArrayList<>());
+		cashBoxRepository.save(cashBoxTest);
+
+		// Needs to have cashBox in constructor?? don;t know why
+		CashBoxStatus cashBoxStatus = new CashBoxStatus(1,19991998.02, location, LocationStatus.DISPATCHED, cashBoxTest);
+		cashBoxTest.updateStatus(cashBoxStatus);
+		cashBoxStatusRepository.save(cashBoxStatus);
 
 	}
 
