3852f6a8cf2328516d9a2595d9c8d233d845d648
Prawploy Techadanai <todaykat@gmail.com>
2022-03-21 14:52:53 +0700

    add repository test for Cash-related entities #12

---
+++ RCTS/src/test/java/com/example/RCTS/cashbox/CashBoxRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.example.RCTS.cashbox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashBoxRepositoryTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
---
+++ RCTS/src/test/java/com/example/RCTS/cashbox/CashBoxStatusRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.RCTS.cashbox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashBoxStatusRepositoryTest {
+
+    @Test
+    void findById() {
+    }
+}
