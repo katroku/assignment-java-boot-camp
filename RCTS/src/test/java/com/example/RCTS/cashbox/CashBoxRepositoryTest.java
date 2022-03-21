8f3a88febf23f850228a9d8fb3559c43e02c6139
Prawploy Techadanai <todaykat@gmail.com>
2022-03-21 16:23:27 +0700

    update CashBox repo test #9

--- RCTS/src/test/java/com/example/RCTS/cashbox/CashBoxRepositoryTest.java
+++ RCTS/src/test/java/com/example/RCTS/cashbox/CashBoxRepositoryTest.java
@@ -1,16 +1,48 @@
 package com.example.RCTS.cashbox;
 
+import com.example.RCTS.address.Geo;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DataJpaTest
 class CashBoxRepositoryTest {
 
+    @Autowired
+    private CashBoxRepository cashBoxRepository;
+
     @Test
-    void findById() {
+    @DisplayName("input: empty object")
+    void findById_with_success() {
+        CashBox cashbox = new CashBox();
+        cashBoxRepository.save(cashbox);
+        Optional<CashBox> result = cashBoxRepository.findById(0);
+        assertTrue(result.isPresent());
     }
 
     @Test
-    void findAll() {
+    @DisplayName("input: none")
+    void findById_with_failure() {
+        Optional<CashBox> result = cashBoxRepository.findById(1234);
+        assertFalse(result.isPresent());
     }
+
+    // There's a problem with findall test
+//    @Test
+//    void findAll_with_success() {
+//        CashBox cashBox = new CashBox();
+//        cashBox.setLocation(null);
+//        cashBox.setStatusList(null);
+//        cashBoxRepository.save(cashBox);
+//        List<CashBox> result = cashBoxRepository.findAll();
+//        assertTrue(result.isEmpty());
+//    }
+
+
 }
