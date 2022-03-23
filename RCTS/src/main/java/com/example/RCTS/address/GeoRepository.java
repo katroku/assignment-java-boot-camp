b711156a1b15a7d082660d7334d457bb6263c3a9
Prawploy Techadanai <todaykat@gmail.com>
2022-03-24 01:24:34 +0700

    edit wrong packagedeclared #10

--- RCTS/src/main/java/com/example/RCTS/address/GeoRepository.java
+++ RCTS/src/main/java/com/example/RCTS/address/GeoRepository.java
@@ -1,6 +1,5 @@
-package com.example.RCTS.cashbox;
+package com.example.RCTS.address;
 
-import com.example.RCTS.address.Geo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
