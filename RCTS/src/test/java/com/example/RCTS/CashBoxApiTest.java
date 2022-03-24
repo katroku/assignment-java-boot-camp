0c4889f5b766faf4f6ae46640fc94255fba0462e
Prawploy Techadanai <todaykat@gmail.com>
2022-03-24 16:10:02 +0700

    update test after adding Branch relationships #9

--- RCTS/src/test/java/com/example/RCTS/CashBoxApiTest.java
+++ RCTS/src/test/java/com/example/RCTS/CashBoxApiTest.java
@@ -2,6 +2,8 @@ package com.example.RCTS;
 
 import com.example.RCTS.address.Geo;
 import com.example.RCTS.address.GeoRepository;
+import com.example.RCTS.branch.Branch;
+import com.example.RCTS.branch.BranchRepository;
 import com.example.RCTS.cash.Cash;
 import com.example.RCTS.cash.CashRepository;
 import com.example.RCTS.cashbox.*;
@@ -41,6 +43,9 @@ class CashBoxApiTest {
     @Autowired
     private CurrencyRepository currencyRepository;
 
+    @Autowired
+    private BranchRepository branchRepository;
+
     @Test
     void api_with_success() {
 
@@ -48,10 +53,20 @@ class CashBoxApiTest {
         Geo location = new Geo(1,latLong); //don't need info about cashboxes or cashbox status
         geoRepository.save(location);
 
+        Branch sender = new Branch(); //don't need info about cashboxes or cashbox status
+        sender.setId(1);
+        branchRepository.save(sender);
+
+        Branch recipient = new Branch(); //don't need info about cashboxes or cashbox status
+        recipient.setId(2);
+        branchRepository.save(recipient);
+
         CashBox cashBoxTest = new CashBox();
         cashBoxTest.setLocation(location);
         cashBoxTest.setStatusList(new ArrayList<>());
         cashBoxTest.setCashList(new ArrayList<>());
+        cashBoxTest.setSender(sender);
+        cashBoxTest.setRecipient(recipient);
         cashBoxRepository.save(cashBoxTest);
 
         // Needs to have cashBox in constructor?? don;t know why
@@ -71,11 +86,11 @@ class CashBoxApiTest {
         //deserialize json to java POJO => done through default constructor in HelloResponse! MUST write explicitly
         CashBoxResponse result = testRestTemplate.getForObject("/cashbox/0", CashBoxResponse.class);
         //Assert
-        assertEquals("{\"id\":0,\"cashList\":[{\"id\":1,\"currency\":{\"id\":444,\"name\":\"DOGE\",\"exchangeRateToBaht\":3000.33},\"amount\":3000.0}],\"location\":{\"id\":1,\"coordinates\":[123.4,123.5]},\"statusList\":[{\"timestamp\":1.999199802E7,\"location\":1,\"locationStatus\":\"DISPATCHED\"}],\"timeCreated\":0}"
+        assertEquals("{\"id\":0,\"cashList\":[{\"id\":1,\"currency\":{\"id\":444,\"name\":\"DOGE\",\"exchangeRateToBaht\":3000.33},\"amount\":3000.0}],\"sender\":{\"id\":1,\"name\":null,\"address\":null,\"totalCashAtBranch\":[]},\"recipient\":{\"id\":2,\"name\":null,\"address\":null,\"totalCashAtBranch\":[]},\"location\":{\"id\":1,\"coordinates\":[123.4,123.5]},\"statusList\":[{\"timestamp\":1.999199802E7,\"location\":1,\"locationStatus\":\"DISPATCHED\"}],\"timeCreated\":0}"
                 ,result.getData());
         CashBoxResponse result2 = testRestTemplate.getForObject("/cashbox", CashBoxResponse.class);
         //Assert
-        assertEquals("[{\"id\":0,\"cashList\":[{\"id\":1,\"currency\":{\"id\":444,\"name\":\"DOGE\",\"exchangeRateToBaht\":3000.33},\"amount\":3000.0}],\"location\":{\"id\":1,\"coordinates\":[123.4,123.5]},\"statusList\":[{\"timestamp\":1.999199802E7,\"location\":1,\"locationStatus\":\"DISPATCHED\"}],\"timeCreated\":0}]"
+        assertEquals("[{\"id\":0,\"cashList\":[{\"id\":1,\"currency\":{\"id\":444,\"name\":\"DOGE\",\"exchangeRateToBaht\":3000.33},\"amount\":3000.0}],\"sender\":{\"id\":1,\"name\":null,\"address\":null,\"totalCashAtBranch\":[]},\"recipient\":{\"id\":2,\"name\":null,\"address\":null,\"totalCashAtBranch\":[]},\"location\":{\"id\":1,\"coordinates\":[123.4,123.5]},\"statusList\":[{\"timestamp\":1.999199802E7,\"location\":1,\"locationStatus\":\"DISPATCHED\"}],\"timeCreated\":0}]"
                 ,result2.getData());
     }
 }
