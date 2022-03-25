fc3acf3789d2c1c965644ff7acbd7240254908e4
Prawploy Techadanai <todaykat@gmail.com>
2022-03-25 16:26:56 +0700

    add more CashBox updating endpoints #9

--- RCTS/src/main/java/com/example/RCTS/cashbox/CashBoxController.java
+++ RCTS/src/main/java/com/example/RCTS/cashbox/CashBoxController.java
@@ -1,8 +1,10 @@
 package com.example.RCTS.cashbox;
 
-import com.example.RCTS.currency.Currency;
+import com.example.RCTS.address.Geo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class CashBoxController {
@@ -30,5 +32,30 @@ public class CashBoxController {
     public CashBox updateCashBox(@RequestBody CashBox newCashBox, @PathVariable int id) {
         return cashBoxRepository.save(newCashBox);
     }
+
+    @PostMapping("/cashbox/{id}/status")
+    public CashBoxStatus updateCashBoxStatus(@RequestBody CashBoxStatus newCashBoxStatus, @PathVariable int id) {
+        return cashBoxRepository.findById(id)
+                .map(cashBox -> {
+                    cashBox.updateStatus(newCashBoxStatus);
+                    cashBoxRepository.save(cashBox);
+                    return newCashBoxStatus;
+                }).orElseGet(() -> {
+                    return newCashBoxStatus;
+                });
+    }
+
+    @PostMapping("/cashbox/{id}/location")
+    public Geo updateCashBoxLocation(@RequestBody Geo newGeo, @PathVariable int id) {
+        return cashBoxRepository.findById(id)
+                .map(cashBox -> {
+                    cashBox.setLocation(newGeo);
+                    cashBoxRepository.save(cashBox);
+                    return newGeo;
+                }).orElseGet(() -> {
+                    return newGeo;
+                });
+    }
+
 
 }
